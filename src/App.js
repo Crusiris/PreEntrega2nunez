235d7aa02798693fbae600d8041d80e69d675af3
Crusiris <nunezcrusiris@gmail.com>
2023-06-26 13:20:27 -0400

    creando componente para itemListContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
- import NavBar from './components/NavBar.js';
+ import ItemListContainer from './components/ItemListContainer.js';
+import NavBar from './components/NavBar.js';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App min-w-sm">
       <NavBar/>
+      <section className='py-10 bg-gray-100'>
+        <div className='mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+          <ItemListContainer/>
+        </div>
+      </section>
     </div>
   );
 }

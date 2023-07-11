760b3550d3a0458c2e5449141d0a72c55018bd3f
Crusiris <nunezcrusiris@gmail.com>
2023-07-11 18:54:47 -0400

    add metodologia BEM para estilos del header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,34 +4,30 @@ import CartWidget from './CartWidget'
 function NavBar() {
   return (
     
-<nav className="bg-white shadow">
-    {/* desktop */}
-  <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+<header className="header">
+  {/* desktop */}
+  <nav className="header__nav nav">
     <div className="flex h-16 justify-between">
       <div className="flex px-2 lg:px-0">
         <div className="flex flex-shrink-0 items-center">
         <div className="flex items-center lg:hidden">
-          <button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
-            
+          <button type="button" className="nav__button--menu" aria-controls="mobile-menu" aria-expanded="false">
+            <span className="sr-only">Open main menu</span> 
             <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-            
+            </svg> 
             <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
-        </div>
-          
+        </div> 
           <img className="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
         </div>
-        <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
-         
-          <a href="#" className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900">Tecnología</a>
-          <a href="#" className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Electrohogar</a>
-          <a href="#" className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Deportes</a>
-          <a href="#" className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Belleza</a>
+        <div className="hidden lg:ml-6 lg:flex lg:space-x-8"> 
+          <a href="#" className="nav__link">Tecnología</a>
+          <a href="#" className="nav__link">Electrohogar</a>
+          <a href="#" className="nav__link">Deportes</a>
+          <a href="#" className="nav__link">Belleza</a>
         </div>
       </div>
       <div className="flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end">
@@ -52,18 +48,18 @@ function NavBar() {
         <CartWidget/> 
       </div>
     </div>
-  </div>
+  </nav>
 
- {/* mobile */}
-  <div className="lg:hidden" id="mobile-menu">
+  {/* mobile */}
+  <nav className="lg:hidden" id="mobile-menu">
     <div className="space-y-1 pb-3 pt-2"> 
-      <a href="#" className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700">Tecnología</a>
-      <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800">Electrohogar</a>
-      <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800">Deportes</a>
-      <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800">Belleza</a>
+      <a href="#" className="nav__link--mob">Tecnología</a>
+      <a href="#" className="nav__link--mob">Electrohogar</a>
+      <a href="#" className="nav__link--mob">Deportes</a>
+      <a href="#" className="nav__link--mob">Belleza</a>
     </div> 
-  </div>
-</nav>
+  </nav>
+</header>
 
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: { 
+        'primary': { 
+          50:'#F9FDF9',
+          100: '#E7F7E8',   
+          500: '#ACE1AF', 
+          900: '#98DA9B',
+        },
+         
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),

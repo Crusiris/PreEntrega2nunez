faa50a8b2ad972d18fbe7cc91035864e4c1e7414
Crusiris <nunezcrusiris@gmail.com>
2023-07-15 15:42:52 -0400

    eliminando css

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -5,7 +5,7 @@ function ItemList({products}) {
   return (
     <>
        {products.length > 0 &&
-            <div className='mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-red-500'> 
+            <div className='mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'> 
                 {products.map(product =>(
                 <Item key={product.id} product={product}/>
                 ))}  
